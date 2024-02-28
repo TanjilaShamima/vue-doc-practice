8f58b7054b506d1b4ea0b21cfa294437ba1b44ff
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2024-02-28 08:29:17 +0600

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<script>
+    import { ref } from "vue";
+    const count = ref(0);
+
+    const increment = () => {
+        count.value++;
+    };
+</script>
+
+
+<template>
+    <button @click="increment">{{count}}</button>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,46 @@
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    const handleChange = (type) => {
+      type === 'inc' ? count.value++ : count.value > 0 && count.value--;
+    };
+    return {
+      count,
+      handleChange,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>The value is: {{ count }}</p>
+    <div class="button-container">
+      <button @click="handleChange('inc')">Increment</button>
+      <button @click="handleChange('dec')">Decrement</button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  button {
+    height: 50px;
+    width: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+.button-container {
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+}
+p {
+  text-align: center;
+}
+</style>
